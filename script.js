1cbacd44f6e663ca1ec336a01fe076ec092b0a15
Joao Vitor Mocambite Lima <86741407+funnyknight18@users.noreply.github.com>
2022-05-13 01:45:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const dummtTransactions = [
+    { id: 1 , name: 'bolo de brigadeiro ', amount: -20 } ,
+    { id: 2 , name: ' salario', amount: 300 } ,
+    { id: 3 , name: ' torta de franco ', amount: -10 } ,
+    { id: 4 , name: ' Violão ', amount: 150 } 
+]
 
+
+const addTransactionIntoDom = transaction => {
+   const operator = transaction.amount < 0 ? '-' : '+'
+   console.log(operator); 
+
+    {/*<li class="minus">
+          Salário <span>-$400</span><button class="delete-btn">x</button>
+  </li>*/} 
+}
