3b3d15929a572da40f43b3bad7f6c6889e2671cb
Joao Vitor Mocambite Lima <86741407+funnyknight18@users.noreply.github.com>
2022-05-14 00:50:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
+const transactionUl = document.querySelector('#transactions');
+
+
 const dummtTransactions = [
     { id: 1 , name: 'bolo de brigadeiro ', amount: -20 } ,
     { id: 2 , name: ' salario', amount: 300 } ,
     { id: 3 , name: ' torta de franco ', amount: -10 } ,
-    { id: 4 , name: ' Violão ', amount: 150 } 
+    { id: 4 , name: ' Violão ', amount: 150 } , 
+    { id: 5 , name: 'arroz ', amount : -25  }
 ]
 
 
 const addTransactionIntoDom = transaction => {
-   const operator = transaction.amount < 0 ? '-' : '+'
-   console.log(operator); 
+   const operator = transaction.amount < 0 ? '-' : '+';
+   const CSSclass = transaction.amount  < 0 ? 'minus' : 'plus'; 
+   const amountWithoutOperator = Math.abs(transaction.amount)
+   const li = document.createElement('li') 
 
-    {/*<li class="minus">
-          Salário <span>-$400</span><button class="delete-btn">x</button>
-  </li>*/} 
+   li.classList.add(CSSclass)
+   li.innerHTML = `
+      ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>
+   `
+   transactionUl.append(li); 
 }
+  
+
+const init = () => {
+   dummtTransactions.forEach(addTransactionIntoDom)
+}
+
+init() 
